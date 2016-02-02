f18a3c24a83f804c5e6247e8fd1c7132b536ef34
Julien <julien@Juliens-MBP.home.gateway>
2016-02-02 15:19:54 +0100

    add previous project

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var elem = document.getElementById("shellcontent");
+elem.style.right = "0px";
+
+var elem2 = document.getElementById("fc_align");
+elem2.style.display = "none";
+
+var elem2 = document.getElementById("theAd");
+elem2.style.display = "none";
